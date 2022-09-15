1adfabbd15e75293530aea9e98d0b7cc7960ec0b
Bushratun Nusaibah <bushratun@gmail.com>
2022-09-14 18:26:24 -0600

    raw URL

--- requests_version.py
+++ requests_version.py
@@ -2,5 +2,10 @@ import requests
 
 #print (requests.__version__)
 
-res = requests.get("http://www.google.com/")
-print(res)
+#res = requests.get("http://www.google.com/")
+#print(res)
+
+raw_url = "https://raw.githubusercontent.com/Bushratun-Nusaibah/CMPUT404/main/requests_version.py"
+
+res = requests.get(raw_url)
+print(res.text)
